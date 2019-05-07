970635e5a9c666c426a24d1c1cf21f1ff6126685
Erik <46297387+ErikPelli@users.noreply.github.com>
2019-05-07 10:55:48 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require("short.php");
+
+echo short-it("https://www.amazon.it"); //It can be whatever Amazon's link
+echo short-uk("https://www.amazon.co.uk");
+echo short-com("https://www.amazon.com");
+echo short-fr("https://www.amazon.fr");
+echo short-de("https://www.amazon.de");
+echo short-es("https://www.amazon.es");
+
+?>
